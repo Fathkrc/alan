98f0839837d2ac51ad9f083c5eb75e8814e99f92
Fatih Karaca <karacaafatih@gmail.com>
2022-12-24 21:17:40 +0300

    v7

--- src/main/java/questions/AlmostIncreasingSequence.java
+++ src/main/java/questions/AlmostIncreasingSequence.java
@@ -1,26 +1,30 @@
 package questions;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class AlmostIncreasingSequence {
     public static void main(String[] args) {
         int[]arr={1,2,3,4,5};
-        List<Integer> list=arr.clone();
-
+        List<Integer>list=new ArrayList<>();
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        Object[] arr2=list.stream().toArray();
     }
-    boolean solution(int[] arr) {
-        if(isWorking(arr)){
-            return true;
-        }else{
-            for(int i=0;i<arr.length;i++){
-
-
-            }
-        }
-    }
-    boolean isWorking(int[]arr){
+   static boolean solution(int[] arr) {
+       List<Integer>list=new ArrayList<>();
+       for (Integer w:arr) {
+           list.add(w);
+       }
+       Collections.sort(list);
+       for(int i=0;i<list.size();i++){
+           if(list.get(i)-list.get(i-1)>1){
+               list.remove(list.indexOf(i));
+           }
+       }
+    return false;}
+ /* static  boolean <E> boolean isWorking(E[]arr){
         for(int i=1;i<arr.length;i++){
             if(arr[i-1]<arr[i]){
                 return true;
@@ -28,5 +32,5 @@ public class AlmostIncreasingSequence {
             else return false;
         }
         return false;
-    }
+    }*/
 }
